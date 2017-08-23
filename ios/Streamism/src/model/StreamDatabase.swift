0ce77e2765ccbabba64c188bcfd79b9b857ea96e
Brian Ault <baultik@gmail.com>
2017-08-22 17:54:48 -0700

    stream data broken into category objects that hold stream objects

--- ios/Streamism/src/model/StreamDatabase.swift
+++ ios/Streamism/src/model/StreamDatabase.swift
@@ -12,17 +12,47 @@ import FirebaseDatabase
 
 class StreamDatabase {
     var ref: DatabaseReference!
-    var handle:UInt
+    var handle:UInt = 0
+    private var masterList = [StreamCategory]()
     
     init() {
         ref = Database.database().reference(withPath: "-KbHuqtKNuu96svHRgjz")
         handle = ref.observe(.value, with: { (snapshot) in
             //print("Streamism data - \(String(describing: snapshot.value))")
-            let dict = snapshot.value as? [String : AnyObject] ?? [:]
-            for category in dict["streams"] as! [[String:String]] {
-                var cat = category["id"]
-                var streams = category["streams"]
+
+            let value = snapshot.value as? [String : AnyObject] ?? [:]
+            let cats = value["streams"] as? [[String: AnyObject]] ?? [[:]]
+            for category in cats {
+                let cat = category["id"] as? String
+                let streams = category["streams"] as? [[String:String]]
+                let streamCat = StreamCategory(type:StreamType(rawValue: cat!)!,data:streams!)
+                self.masterList.append(streamCat)
             }
         })
+    }
+    
+    
+    
+    func shuffle(list:[StreamCategory], categories:[StreamType]) -> [Stream] {
+        var filteredList = [Stream]()
+        var filteredCats = [StreamCategory]()
+        
+        //filter out cats if necessary - may move this and pass in filtered list
+        for type in categories {
+            for cat in list {
+                if type.rawValue == cat.type.rawValue {
+                    filteredCats.append(cat)
+                }
+            }
+        }
+        
+        //get max length
+        
+        //Sort 1 of each cat at a time - better way?
+        //go through each category - make temp array holding i element of each
+        //random sort temp array
+        //add array elements to final list
+        
+        return filteredList
     }
 }
